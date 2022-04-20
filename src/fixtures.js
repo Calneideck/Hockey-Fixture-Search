515696a5b1940b3c7d6a3b2f410ba00593a2b59c
Callum Neideck <cal_neideck@hotmail.com>
2022-04-20 12:05:00 +1000

    Updated CRA and react

--- src/fixtures.js
+++ src/fixtures.js
@@ -11,14 +11,17 @@ export const useGetFixtures = () => {
           const data = await res.text();
           const rows = data.split('\n').map(row => row.split(','))
           const sortedData = rows.sort((a, b) => {
-            if (isNaN(a[2]) && isNaN(b[2]))
-              return 0;
-            else if (isNaN(a[2]) && !isNaN(b[2]))
-              return 1;
-            else if (!isNaN(a[2]) && isNaN(b[2]))
-              return -1;
-            else
-              return a[2] - b[2];
+            if (a[0] === b[0]) {
+              if (isNaN(a[2]) && isNaN(b[2]))
+                return 0;
+              else if (isNaN(a[2]) && !isNaN(b[2]))
+                return 1;
+              else if (!isNaN(a[2]) && isNaN(b[2]))
+                return -1;
+              else
+                return a[2] - b[2];
+            }
+            return a[0] - b[0]
           })
           setFixtures({ state: 'data', data: sortedData })
         }
